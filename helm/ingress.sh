93025025ccd1b5a96f3377919b334617f802668d
Fan Yang <fan.yang-turner@bristol.ac.uk>
2024-12-06 12:37:03 +0000

    deploy ingress

---
+++ helm/ingress.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install nginx-ingress ingress-nginx/ingress-nginx \
+  --namespace ingress-nginx \
+  --create-namespace \
+  --set controller.service.type=NodePort
